51ff0c7865fc1d908ee60562cc85c1910a71cc32
dkssu <dkssudrhd@naver.com>
2024-10-30 22:55:22 +0900

    feature : init_setting

---
+++ src/test/java/com/hancloud/hancloud/HanCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hancloud.hancloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
